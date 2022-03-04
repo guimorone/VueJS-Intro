6e118b44bf2c9cbcf28c103ae0db43195453ee81
Guilherme Morone Araujo <guimorone@gmail.com>
2022-03-04 00:52:51 -0300

    bootstrap adicionado

--- projetinho/vue-samurai/src/main.js
+++ projetinho/vue-samurai/src/main.js
@@ -1,5 +1,10 @@
-import { createApp } from 'vue'
+import 'mutationobserver-shim'
+import Vue from 'vue'
+import './plugins/bootstrap-vue'
 import App from './App.vue'
-import router from './router'
 
-createApp(App).use(router).mount('#app')
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
